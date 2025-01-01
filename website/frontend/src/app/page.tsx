2848ae68898bb1f473753040103c99c4298f5bb7
Amol Kelkar <kelkar.amol@gmail.com>
2025-01-01 06:02:00 -0800

    Keep gradient with content

--- website/frontend/src/app/page.tsx
+++ website/frontend/src/app/page.tsx
@@ -49,7 +49,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-12">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-12 relative">
       <div className={styles.gradientBackground}></div>
       {/* Hero Section */}
       <div className="w-full max-w-5xl">
