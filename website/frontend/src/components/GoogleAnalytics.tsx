45505fef759c01d59494e5cf9692f1d7748e9d92
Amol Kelkar <kelkar.amol@gmail.com>
2025-01-01 21:15:00 -0800

    Google analytics

---
+++ website/frontend/src/components/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-V3Q6X91ZXZ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-V3Q6X91ZXZ');
+        `}
+      </Script>
+    </>
+  );
+}
