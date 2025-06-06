4d50de153c9e0f03e8c12da029a2eae22a750753
Amol Kelkar <kelkar.amol@gmail.com>
2025-06-06 12:17:06 -0700

    Fix tests for new logging format

--- tests/unit/playbooks/test_program_begin.py
+++ tests/unit/playbooks/test_program_begin.py
@@ -22,9 +22,9 @@ async def test_begin(playbooks):
     await playbooks.program.run_till_exit()
     agent1_session_log = playbooks.program.agents[0].state.session_log.to_log_full()
     assert "python_a1p2() finished" in agent1_session_log
-    assert "A1P2() finished" in agent1_session_log
-    assert "A1P3() finished" in agent1_session_log
+    assert "SendMessage(human, A1P2)" in agent1_session_log
+    assert "SendMessage(human, A1P3)" in agent1_session_log
 
     agent2_session_log = playbooks.program.agents[1].state.session_log.to_log_full()
-    assert "A2P1() finished" in agent2_session_log
-    assert "A2P2() finished" in agent2_session_log
+    assert "SendMessage(human, A2P1)" in agent2_session_log
+    assert "SendMessage(human, A2P2)" in agent2_session_log
