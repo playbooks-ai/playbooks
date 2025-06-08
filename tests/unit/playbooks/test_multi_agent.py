ee325d4f72159c258a0e318325676fb3709776a0
Amol Kelkar <kelkar.amol@gmail.com>
2025-06-08 14:38:21 -0700

    Fix test

--- tests/unit/playbooks/test_multi_agent.py
+++ tests/unit/playbooks/test_multi_agent.py
@@ -16,11 +16,9 @@ async def test_multi_agent(test_data_dir):
     assert "A(1024) → 32.0" in log
 
     country_info_agent = playbooks.program.agents[1]
-    log = country_info_agent.state.sesson.to_log_full()
+    log = country_info_agent.state.session_log.to_log_full()
     assert "GetCountrySecret(Canada) → " in log
     assert "FirstAgent.A(num=5) → 2.23" in log
     assert "GetCountryPopulation(India) → 2.23" in log
     assert "FirstAgent.A(1024) → 32.0" in log
     assert "Main() → Execution finished" in log
-
-    assert 10 == 10
